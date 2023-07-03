19803f833bba9c937e6427c38348a3c9be488c16
DannyG25 <dannyy_2000@hotmail.com>
2023-07-03 11:53:11 -0500

    change icons view

--- angular-frontend/src/app/layout/app.menu.component.ts
+++ angular-frontend/src/app/layout/app.menu.component.ts
@@ -23,33 +23,33 @@ export class AppMenuComponent implements OnInit {
             {
                 label: 'Administrator',
                 items: [
-                    { label: 'Companies', icon: 'pi pi-fw pi-home', routerLink: ['/administrator/company-crud'] },
-                    { label: 'Users', icon: 'pi pi-fw pi-home', routerLink: ['/administrator/users'] },
+                    { label: 'Companies', icon: 'pi pi-fw pi-building', routerLink: ['/administrator/company-crud'] },
+                    { label: 'Users', icon: 'pi pi-fw pi-users', routerLink: ['/administrator/users'] },
                 ]
             },
             {
                 label: 'Managament',
                 items: [
-                    { label: 'Location', icon: 'pi pi-fw pi-home', routerLink: ['/management/location'] },
-                    { label: 'employee', icon: 'pi pi-fw pi-home', routerLink: ['/management/employee'] },
-                    { label: 'device', icon: 'pi pi-fw pi-home', routerLink: ['/management/device'] },
-                    { label: 'tax', icon: 'pi pi-fw pi-home', routerLink: ['/management/tax'] },
-                    { label: 'category', icon: 'pi pi-fw pi-home', routerLink: ['/management/category'] },
-                    { label: 'Unit Measure', icon: 'pi pi-fw pi-home', routerLink: ['/management/unitMeasure'] },
-                    { label: 'Products', icon: 'pi pi-fw pi-home', routerLink: ['/management/product'] },
-                    { label: 'Stock', icon: 'pi pi-fw pi-home', routerLink: ['/management/stock'] },
+                    { label: 'Location', icon: 'pi pi-fw pi-map', routerLink: ['/management/location'] },
+                    { label: 'employee', icon: 'pi pi-fw pi-users', routerLink: ['/management/employee'] },
+                    { label: 'device', icon: 'pi pi-fw pi-server', routerLink: ['/management/device'] },
+                    { label: 'tax', icon: 'pi pi-fw pi-percentage', routerLink: ['/management/tax'] },
+                    { label: 'category', icon: 'pi pi-fw pi-tags', routerLink: ['/management/category'] },
+                    { label: 'Unit Measure', icon: 'pi pi-fw pi-pencil', routerLink: ['/management/unitMeasure'] },
+                    { label: 'Products', icon: 'pi pi-fw pi-gift', routerLink: ['/management/product'] },
+                    { label: 'Stock', icon: 'pi pi-fw pi-th-large', routerLink: ['/management/stock'] },
                     {
                         label: 'billing',
-                        icon: 'pi pi-fw pi-user',
+                        icon: 'pi pi-fw pi-list',
                         items: [
                             {
                                 label: 'New Transaction',
-                                icon: 'pi pi-fw pi-home',
+                                icon: 'pi pi-fw pi-shopping-cart',
                                 routerLink: ['/management/transaction']
                             },
                             {
                                 label: 'List Transactions',
-                                icon: 'pi pi-fw pi-home',
+                                icon: 'pi pi-fw pi-shopping-bag',
                                 routerLink: ['/management/transactionlist']
                             },
                             
@@ -57,16 +57,16 @@ export class AppMenuComponent implements OnInit {
                     },
                     {
                         label: 'Inventory',
-                        icon: 'pi pi-fw pi-user',
+                        icon: 'pi pi-fw pi-list',
                         items: [
                             {
                                 label: 'Calculate Inventory',
-                                icon: 'pi pi-fw pi-home',
+                                icon: 'pi pi-fw pi-file-edit',
                                 routerLink: ['/management/inventory']
                             },
                             {
                                 label: 'List Inventory',
-                                icon: 'pi pi-fw pi-home',
+                                icon: 'pi pi-fw pi-file',
                                 routerLink: ['/management/inventorylist']
                             },
                             
